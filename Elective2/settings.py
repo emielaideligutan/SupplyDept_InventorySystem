b87728c45f97532a8310a89382bf77934375130a
WendelAlvero <wendel.alvero@gsfe.tupcavite.edu.ph>
2021-10-26 22:33:09 +0800

    editlang sa settings.py

--- Elective2/settings.py
+++ Elective2/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'SupplyDept_Inventory.apps.SupplydeptInventoryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
