0e2ece098e24a96faa721ccaf1bcad43e2590595
WendelAlvero <wendel.alvero@gsfe.tupcavite.edu.ph>
2021-10-26 22:44:56 +0800

    edit views.py

--- SupplyDept_Inventory/views.py
+++ SupplyDept_Inventory/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def login(request):
-    return HttpResponse("LOGIN")
+    return render(request, 'activities/login.html')
