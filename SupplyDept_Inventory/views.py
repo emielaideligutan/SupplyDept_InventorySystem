6ca0ea44427a5312510c6b107b44e36feb0ffc4d
emielaideligutan <91707627+emielaideligutan@users.noreply.github.com>
2021-10-27 13:36:35 +0800

    Update views.py

--- SupplyDept_Inventory/views.py
+++ SupplyDept_Inventory/views.py
@@ -9,3 +9,9 @@ def mainpage(request):
     
 def delivery(request):
     return render(request, 'activities/delivery.html')
+
+def withdraw(request):
+    return render(request, 'activities/withdraw.html')
+
+def status(request):
+    return render(request, 'activities/status.html')
