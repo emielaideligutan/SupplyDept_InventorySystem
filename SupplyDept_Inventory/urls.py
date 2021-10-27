b600bfe99761fe7d7367a9a0ecf1397668126632
emielaideligutan <91707627+emielaideligutan@users.noreply.github.com>
2021-10-27 13:39:13 +0800

    Update urls.py

--- SupplyDept_Inventory/urls.py
+++ SupplyDept_Inventory/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.login, name='Supplydept_login'),
     path('mainpage/', views.mainpage, name='Supplydept_mainpage'),
     path('delivery/', views.delivery, name='Supplydept_delivery'),
+    path('withdraw/', views.withdraw, name='Supplydept_withdraw'),
+    path('status/', views.status, name='Supplydept_status'),
+
+
 
 ]
