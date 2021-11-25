9ba4f101164bff24c72413af0432d9423ce962c0
emielaideligutan <emielaide.ligutan@gsfe.tupcavite.edu.ph>
2021-11-25 21:27:12 +0800

    Elective 2

--- SupplyDept_Inventory/models.py
+++ SupplyDept_Inventory/models.py
@@ -39,3 +39,35 @@ class mainstorage(models.Model):
 
 def __str__(self):
     return self.ItemName
+
+class OCS_CASHIER_SERVICES(models.Model):
+    ItemName = models.CharField(max_length=50, verbose_name='item name')
+    Unit = models.CharField(max_length=50, verbose_name='unit')
+    Quantity = models.CharField(max_length=50, verbose_name='quantity')
+
+def __str__(self):
+    return self.ItemName
+
+class OES_EXTENSION_SERVICES(models.Model):
+    ItemName = models.CharField(max_length=50, verbose_name='item name')
+    Unit = models.CharField(max_length=50, verbose_name='unit')
+    Quantity = models.CharField(max_length=50, verbose_name='quantity')
+
+def __str__(self):
+    return self.ItemName
+
+class OGS_GUIDANCE_SERVICES(models.Model):
+    ItemName = models.CharField(max_length=50, verbose_name='item name')
+    Unit = models.CharField(max_length=50, verbose_name='unit')
+    Quantity = models.CharField(max_length=50, verbose_name='quantity')
+
+def __str__(self):
+    return self.ItemName
+    
+class OHR_HUMAN_RESOURCES(models.Model):
+    ItemName = models.CharField(max_length=50, verbose_name='item name')
+    Unit = models.CharField(max_length=50, verbose_name='unit')
+    Quantity = models.CharField(max_length=50, verbose_name='quantity')
+
+def __str__(self):
+    return self.ItemName
