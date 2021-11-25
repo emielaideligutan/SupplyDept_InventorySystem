8ed55ebedce994b0a92d035878fe5cf1618f5800
emielaideligutan <emielaide.ligutan@gsfe.tupcavite.edu.ph>
2021-11-25 21:08:33 +0800

    Elective 2

--- Elective2/settings.py
+++ Elective2/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'Elective2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'supplydept_inventory',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
     }
 }
 
@@ -119,6 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- SupplyDept_Inventory/models.py
+++ SupplyDept_Inventory/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class admin(models.Model):
+
+    Admin_Username = models.CharField(max_length=50, verbose_name='admin username')
+    Admin_Password = models.CharField(max_length=50, verbose_name='admin password')
+
+def __str__(self):
+    return self.Admin_Username
+
+class withdrawrecords(models.Model):
+
+    ItemName = models.CharField(max_length=50, verbose_name='item name')
+    Unit = models.CharField(max_length=50, verbose_name='unit')
+    Quantity = models.CharField(max_length=50, verbose_name='quantity')
+    current_date = models.CharField(max_length=50, verbose_name='current date')
+    current_time = models.CharField(max_length=50, verbose_name='current time')
+
+def __str__(self):
+    return self.ItemName
+
+
+class deliveryrecords(models.Model):
+    ItemName = models.CharField(max_length=50, verbose_name='item name')
+    Unit = models.CharField(max_length=50, verbose_name='unit')
+    Quantity = models.CharField(max_length=50, verbose_name='quantity')
+    current_date = models.CharField(max_length=50, verbose_name='current date')
+    current_time = models.CharField(max_length=50, verbose_name='current time')
+
+def __str__(self):
+    return self.ItemName
+
+
+class mainstorage(models.Model):
+    ItemName = models.CharField(max_length=50, verbose_name='item name')
+    Unit = models.CharField(max_length=50, verbose_name='unit')
+    Quantity = models.CharField(max_length=50, verbose_name='quantity')
+
+def __str__(self):
+    return self.ItemName
