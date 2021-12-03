0eebbd448e6dc2fe97984de6c10c351d02f98900
emielaideligutan <emielaide.ligutan@gsfe.tupcavite.edu.ph>
2021-12-03 18:52:25 +0800

    Elective 2

--- SupplyDept_Inventory/views.py
+++ SupplyDept_Inventory/views.py
@@ -19,26 +19,49 @@ def contact(request):
     return render(request, 'activities/contact.html')
     
 def delivery(request):
+    information = deliveryrecords.objects.all()
+    if 'delivery_item_name' in request.POST:
+        text = request.POST['delivery_item_name']
+        if text == '':
+            information = []
+        else:
+            print('none')
+
     if request.method == "POST":
         if request.POST.get('delivery_item_name') and request.POST.get('delivery_unit') and request.POST.get('delivery_quantity'):
-           save_delivery_record = deliveryrecords()
-           save_delivery_record.delivery_item_name = request.POST.get('delivery_item_name')
-           save_delivery_record.delivery_unit = request.POST.get('delivery_unit')
-           save_delivery_record.delivery_quantity = request.POST.get('delivery_quantity')
-           save_delivery_record.save()
-           save_main_storage = mainstorage()
-           save_main_storage.ItemName = request.POST.get('delivery_item_name')
-           save_main_storage.Quantity = request.POST.get('delivery_quantity')
-           save_main_storage.Unit = request.POST.get('delivery_unit')
-           save_main_storage.save()
+            save_delivery_record = deliveryrecords()
+            save_delivery_record.delivery_item_name = request.POST.get('delivery_item_name')
+            save_delivery_record.delivery_unit = request.POST.get('delivery_unit')
+            save_delivery_record.delivery_quantity = request.POST.get('delivery_quantity')
+            save_delivery_record.save()
+            save_main_storage = mainstorage()
+            save_main_storage.ItemName = request.POST.get('delivery_item_name')
+            save_main_storage.Quantity = request.POST.get('delivery_quantity')
+            save_main_storage.Unit = request.POST.get('delivery_unit')
+            save_main_storage.save()
+        else:
+            return render (request, 'activities/delivery.html')
 
-           return render (request, 'activities/delivery.html')
-
-    else:
-        return render (request, 'activities/delivery.html')
+    context = {
+        'information' : information,
+        }
+    return render (request, 'activities/delivery.html', context)
+    
 
 def withdraw(request):
-    return render(request, 'activities/withdraw.html')
+    result = mainstorage.objects.all()
+    if 'delivery_item_name' in request.POST:
+        text = request.POST['delivery_item_name']
+        if text == '':
+            result = []
+        else:
+            print('none')
+
+    context = {
+        'information' : result,
+        }
+    return render (request, 'activities/withdraw.html', context)
+    
 
 def tempwithdraw(request):
     return render(request, 'activities/tempwithdraw.html')
