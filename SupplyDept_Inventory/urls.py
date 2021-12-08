fce9df802ad4ef250563ec84bafbbbf4715d5dcc
reymiemadera <93207567+reymiemadera@users.noreply.github.com>
2021-12-08 21:58:49 +0800

    Update urls.py

--- SupplyDept_Inventory/urls.py
+++ SupplyDept_Inventory/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('delivery/', views.delivery, name='Supplydept_delivery'),
     path('withdraw/', views.withdraw, name='Supplydept_withdraw'),
     path('status/', views.status, name='Supplydept_status'),
-    path('about/', views.status, name='Supplydept_about'),
-    path('contact/', views.status, name='Supplydept_contact'),
-    path('tempwithdraw/', views.status, name='Supplydept_tempwithdraw'),
+    path('about/', views.about, name='Supplydept_about'),
+    path('contact/', views.contact, name='Supplydept_contact'),
+    path('tempwithdraw/', views.tempwithdraw, name='Supplydept_tempwithdraw'),
 
 ]
