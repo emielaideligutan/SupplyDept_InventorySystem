0a1a8def1af2e5895d9e37fa74107a4ff2853d1a
WendelAlvero <wendel.alvero@gsfe.tupcavite.edu.ph>
2021-12-10 13:18:41 +0800

    Elective2

--- SupplyDept_Inventory/views.py
+++ SupplyDept_Inventory/views.py
@@ -29,8 +29,8 @@ def delivery(request):
 
     if request.method == "POST":
         if request.POST.get('delivery_item_name') and request.POST.get('delivery_unit') and request.POST.get('delivery_quantity'):
-            information1 = mainstorage.objects.get(ItemName = request.POST.get('delivery_item_name'))
-            if mainstorage.objects.filter(ItemName = request.POST.get('delivery_item_name')).exists() == True and information1.Unit == request.POST.get('delivery_unit'):
+            if mainstorage.objects.filter(ItemName = request.POST.get('delivery_item_name')).exists() == True and mainstorage.objects.get(ItemName = request.POST.get('delivery_item_name')).Unit == request.POST.get('delivery_unit'):
+                information1 = mainstorage.objects.get(ItemName = request.POST.get('delivery_item_name'))
                 updating = int(information1.Quantity) + int(request.POST.get('delivery_quantity'))
                 update_mainstorage = mainstorage()
                 update_delivery_record = deliveryrecords()
@@ -48,7 +48,7 @@ def delivery(request):
                 update_delivery_record.save()
 
    
-            elif mainstorage.objects.filter(ItemName = request.POST.get('delivery_item_name')).exists() == False or information1.Unit != request.POST.get('delivery_unit'):
+            elif mainstorage.objects.filter(ItemName = request.POST.get('delivery_item_name')).exists() == False or mainstorage.objects.get(ItemName = request.POST.get('delivery_item_name')).Unit != request.POST.get('delivery_unit'):
                 save_delivery_record = deliveryrecords()
                 save_delivery_record.delivery_item_name = request.POST.get('delivery_item_name')
                 save_delivery_record.delivery_unit = request.POST.get('delivery_unit')
